6fa2e67b1df72c852c7f12af9b5b5cf3b808cf00
cjonuonga <calebonuonga@yahoo.com>
2026-02-22 13:05:08 -0800

    Created map, got rid of unecessary/unused files, setup character movement.

--- Generic_Home_1_Layer_1.tsx
+++ Generic_Home_1_Layer_1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Generic_Home_1_Layer_1" tilewidth="16" tileheight="16" tilecount="182" columns="14">
- <image source="public/moderninteriors-win/6_Home_Designs/Generic_Home_Designs/16x16/Generic_Home_1_Layer_1.png" width="224" height="214"/>
+ <image source="public/Generic_Home_1_Layer_1.png" width="224" height="214"/>
 </tileset>
--- src/main.js
+++ src/main.js
@@ -1,2 +1,27 @@
 import { k } from "./kaboomCtx";
-k.loadSprite(sprite);
+
+k.loadSprite("Generic_Home_1_Layer_1", "Generic_Home_1_Layer_1.png", {
+  sliceX: 14,
+  sliceY: 13,
+});
+
+k.loadSprite("Generic_Home_1_Layer_2", "Generic_Home_1_Layer_2_.png", {
+  sliceX: 14,
+  sliceY: 13,
+});
+
+k.loadSprite("samuel", "Samuel_16x16.png", {
+  sliceX: 48,
+  sliceY: 18,
+  anims: {
+    // Idle animations (short loops)
+    "idle-down": { from: 0, to: 5, loop: true, speed: 3 },
+    "idle-up": { from: 6, to: 11, loop: true, speed: 3 },
+    "idle-side": { from: 12, to: 17, loop: true, speed: 3 },
+
+    // Walk animations
+    "walk-down": { from: 48, to: 53, loop: true, speed: 8 },
+    "walk-up": { from: 96, to: 101, loop: true, speed: 8 },
+    "walk-side": { from: 144, to: 149, loop: true, speed: 8 },
+  },
+});
